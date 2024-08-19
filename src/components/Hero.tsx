00ba9f760fbc1689990f0f87c9e8425e3da43a62
bshongwe <dru.da.beat.hustle@gmail.com>
2024-08-19 17:41:27 +0200

    Update UI: buttons and content review

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,7 +33,7 @@ export const Hero = () => {
       </div>
       </div>
       <div className="flex justify-center">
-      <p className='text-xl text-center mt-8 max-w-md'>Transform your learning using our AI-powered flashcards. Tailored to help you master any set of concepts effortlessly.</p>
+      <p className='text-xl text-center mt-8 max-w-md'>Accelerate your learning using our AI-powered flashcards. Tailored to help you master any set of concepts effortlessly.</p>
       </div>
       <div className="flex justify-center mt-8">
       <button className='bg-white text-black py-3 px-5 rounded-lg font-medium'>Sign up</button>
