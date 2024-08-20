ac7d626663eaa3174daefbd4c74c909f22effb4b
bshongwe <dru.da.beat.hustle@gmail.com>
2024-08-20 15:14:03 +0200

    Waitlist modal import correction

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -4,7 +4,7 @@ import EmojiImage from '../assets/images/emojistar.png'
 import Image from 'next/image';
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { use, useRef } from 'react';
-import Waitlist from './Waitlist.jsx';
+import Waitlist from './Waitlist.tsx';
 
 export const CallToAction = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -33,7 +33,7 @@ export const CallToAction = () => {
         <p className="text-xl text-white/70  mt-5">Join hundreds of users that are anticipating being part of Recalla Pro set for release soon. Are you <i>flashcard game?</i></p>
         <form className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row">
           <input type="email" placeholder="shongwe.bhekizwe@gmail.com" className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF] sm:flex-1"/>
-          <button className="bg-white text-black h-12 rounded-lg px-5" onClick={handleOpenModal}>Join Waitlist</button>
+          <button type="submit" className="group inline-flex items-center justify-center text-center text-sm leading-loose bg-purple-500 text-black h-12 rounded-lg px-5" onClick={handleOpenModal}>✨ <span className="font-bold">Join Waitlist</span> <FaChevronRight className="ml-1 size-4 transition-all duration-300 ease-out group-hover:translate-x-1" /></button>
         </form>
       </div>
 
